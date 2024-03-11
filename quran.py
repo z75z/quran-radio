667132a371ec805e5a6ba1b280caf0eb53805511
z75z <110237161+z75z@users.noreply.github.com>
2024-03-11 16:10:39 +0300

    Update quran.py

--- quran.py
+++ quran.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 from pytgcalls import idle
-from pytgcalls.types import AudioPiped
+from pytgcalls.types import MediaStream
 
 from pyrogram.raw.functions.channels import GetFullChannel
 from pyrogram.raw.functions.phone import EditGroupCallTitle
@@ -47,12 +47,12 @@ async def Call():
                     if not CHANNEL_USERNAMWE:
                         await call.join_group_call(
                             CHAT_ID,
-                            AudioPiped(surah_url),
+                            MediaStream(surah_url),
                         )
                     else:
                         await call.join_group_call(
                             CHAT_ID,
-                            AudioPiped(surah_url),
+                            MediaStream(surah_url),
                             join_as=await app.resolve_peer(CHANNEL_USERNAMWE)
                         )
                     channel = await app.invoke(GetFullChannel(channel=await app.resolve_peer(CHAT_ID)))
